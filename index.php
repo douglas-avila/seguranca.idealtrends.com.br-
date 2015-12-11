81779b5076117d4eaf782f2cfd054abafb5368bd
douglasavilac2 <douglas-avila@live.com>
2015-12-11 14:17:28 +0000

    Novo implementação

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-Douglas FIT 2git 
+<?php 
+    $urls = array(
+        'asafira.com.br',
+        'solucoesindustriais.com.br'
+    );
+    
+    t34t43t34t34t34
+    foreach($urls as $url){
+        $content = file_get_contents($url);
+        
+        echo $content;
+    }
+?>
